1fa526dab08942b9ed6b1914476f06063c725ea4
James Massara <james_massara@intuit.com>
2013-08-15 09:58:32 -0700

    updated comments/doc

--- quickbase.go
+++ quickbase.go
@@ -1,4 +1,10 @@
+// Copyright 2013 James Massara. All rights reserved.
+
+// Package quickbase implements pieces of the Quickbase API.
 package quickbase
+
+// References:
+//    QuickBase API: http://www.quickbase.com/api-guide/index.html.
 
 import (
 	"bytes"
@@ -7,22 +13,26 @@ import (
 	"net/http"
 )
 
+// QuickBase represents a QuickBase domain.
 type QuickBase struct {
 	Domain string
 }
 
+// New creates a new QuickBase.
 func New(domain string) *QuickBase {
 	return &QuickBase{Domain: domain}
 }
 
+// Send an XML API request to QuickBase and populate `response' with the XML
+// response returned from QuickBase.
 func (qb *QuickBase) query(params map[string]string, request, response interface{}) error {
 	postdata, err := xml.MarshalIndent(request, "", " ")
 	if err != nil {
 		return err
 	}
 
-	// Convert `&#39;' to `&apos;' because xml.Marshal runs EscapeText which
-	// and transforms `'' to `&#39;', fine for HTML but not XML.
+	// Convert `&#39;' to `&apos;' because xml.Marshal runs EscapeText and
+	// transforms `'' to `&#39;', which is fine for HTML but not XML.
 	postdata = bytes.Replace(postdata, []byte("&#39;"), []byte("&apos;"), -1)
 
 	req, err := http.NewRequest("POST", params["url"], bytes.NewBuffer(postdata))
@@ -46,6 +56,7 @@ func (qb *QuickBase) query(params map[string]string, request, response interface
 	return xml.NewDecoder(res.Body).Decode(response)
 }
 
+// QuickBaseError represents a detailed error message returned by the QB API
 type QuickBaseError struct {
 	msg    string
 	Detail string
@@ -55,7 +66,7 @@ func (e *QuickBaseError) Error() string { return e.msg }
 
 // Helper functions
 
-// makeParams returns the initial map with the action set.
+// makeParams returns an initial map with the action set.
 func makeParams(action string) map[string]string {
 	params := make(map[string]string)
 	params["action"] = action
